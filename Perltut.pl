a5bfa0c117bdf065c1c053a8c154c37cdd38d1c2
unknown <kaceltahar@gmail.com>
2020-05-23 15:48:09 +0200

    now

---
+++ Perltut.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use diagnostics;
+use feature 'say'; #used for not doing qottes
+use feature 'switch';
+
+###################First Perl Lab###########################################
+my $name = "tahar";
+my ($ages,$adress) = (25,"35 rue Michelet Valenton 94460"); 
+my $Presentation =qq{$name is living on $adress and has $ages years old \n};
+say $Presentation;
+#say "5+4 = ",5+4,"\n";
+##################Seconde Perl Lab##########################################
+my $age = 80;
+my $is_Not_Intoxicated = 1;
+my $ageLastExam = 16;
+
+if($age<16){
+say you cant drive;
+}
+elsif(!$is_Not_Intoxicated){
+
+}
+}
+
+
+
+
