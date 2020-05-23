1f8074d65b600f30a5d953432691d4a65475236e
unknown <kaceltahar@gmail.com>
2020-05-23 20:40:50 +0200

    hello aws

--- Perltut.pl
+++ Perltut.pl
@@ -6,23 +6,40 @@ use feature 'say'; #used for not doing qottes
 use feature 'switch';
 
 ###################First Perl Lab###########################################
-my $name = "tahar";
-my ($ages,$adress) = (25,"35 rue Michelet Valenton 94460"); 
-my $Presentation =qq{$name is living on $adress and has $ages years old \n};
-say $Presentation;
+#my $name = "tahar";
+#my ($ages,$adress) = (25,"35 rue Michelet Valenton 94460");
+#my $Presentation =qq{$name is living on $adress and has $ages years old \n};
+#say $Presentation;
 #say "5+4 = ",5+4,"\n";
 ##################Seconde Perl Lab##########################################
-my $age = 80;
-my $is_Not_Intoxicated = 1;
-my $ageLastExam = 16;
+#my $age = 80;
+#my $is_Not_Intoxicated = 1;
+#my $ageLastExam = 16;
 
-if($age<16){
-say you cant drive;
-}
-elsif(!$is_Not_Intoxicated){
+#if($age<16){
+#say "you cant drive";
+#}
+#elsif(!$is_Not_Intoxicated){
+#say "hello World";
+#}
+my $test;
+$test = <STDIN>;
+my $i = 1;
+while ($test != 5 && $i < 6){ 
+    my $test = <STDIN>;
+if ($test == 10){
+    #say "hello Tahar";
+    print "hello Tahar";
+    exit;
+                }
+else{
+    print "no!!!!";
+    
+    }
+ $i=$i++;
 
 }
-}
+
 
 
 
